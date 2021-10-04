ed8a7b4fd5e7bafce76b69a5793aa3fe166f8abc
Shrutihindocha <90174067+Shrutihindocha@users.noreply.github.com>
2021-10-04 18:17:35 +0800

    rev 4

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,50 +10,48 @@ changes = []
 change = 0
 profit_losses = []
 
-#Defining function for statistical anaylsis
-def financial_analysis(budgetdata):
-
-    #The total number of months included in the dataset
-    date = datetime.strptime(budgetdata[0],'%b-%Y')
-    months.append(date.month)
-    months_total = len(months)
-
-    #The net total amount of "Profit/Losses" over the entire period
-    profit_losses.append(float(budgetdata[1]))
-    net = sum(profit_losses)
-
-    #Average of the changes
-    profit_loss = budgetdata[1]
-    change = profit_losses - change
-    changes.append(change)
-    change = profit_losses
-    
-    #The average of the changes in "Profit/Losses" over the entire period
-    total_change = sum(changes)
-    average_change = total_change/len(changes)
-
-    #The greatest increase in profits (date and amount) over the entire period
-    gincrease_prof = changes.max()
-    gincrease_date = budgetdata[0]
-    
-    #The greatest decrease in losses (date and amount) over the entire period
-    gdecrease_loss = changes.min()
-    gdecrease_date = budgetdata[0]
-
 #opening and reading budget_data.csv input file
 with open(filepath_read, 'r') as budgetdata_csv:
     csvreader = csv.reader(budgetdata_csv, delimiter = ',')
     csvheader = next(csvreader)
 
-    for rows in csvreader: 
-        financial_analysis(rows)
+    for row in csvreader: 
+    #The total number of months included in the dataset
+        date = datetime.strptime(row[0],'%b-%Y')
+        months.append(date.month)
+        months_total = len(months)
+
+        #The net total amount of "Profit/Losses" over the entire period
+        profit_losses.append(float(row[1]))
+        net = sum(profit_losses)
+
+        #Average of the changes
+        profit_loss = float(row[1])
+        change = profit_loss - change
+        changes.append(change)
+        #The greatest increase in profits (date and amount) over the entire period
+        gincrease_prof = max(changes)
+        if change == max(changes):  
+            gincrease_date = row[0]
+        #The greatest decrease in losses (date and amount) over the entire period
+        gdecrease_loss = min(changes)
+        if change == min(changes):
+            gdecrease_date = row[0]
+        change = profit_loss
+        
+    #The average of the changes in "Profit/Losses" over the entire period
+    del changes[0]
+    total_change = sum(changes)
+    average_change = total_change/len(changes)
+
+        
 
 #prining results to the terminal window
 print(f'Financial Analysis')
 print('--------------------------------')
 print(f'Total Months: {months_total}')
 print(f'Total: {net}')
-print(f'Averange change: {average_change}')
+print(f'Averange change: {round(average_change,2)}')
 print(f'Greatest Increase in Profits: {gincrease_date} (${gincrease_prof})')
 print(f'Greatest Decrease in Losses: {gdecrease_date} (${gdecrease_loss})')
 
@@ -61,13 +59,13 @@ print(f'Greatest Decrease in Losses: {gdecrease_date} (${gdecrease_loss})')
 filepath_write = os.path.join("Analysis", "output.csv")
 
 with open(filepath_write, 'w', newline='') as txtfile:
-    outputxt = csv.writer(txtfile, delimiter= '')
+    outputxt = csv.writer(txtfile)
     #writing rows
     outputxt.writerow('Financial Analysis')
     outputxt.writerow('--------------------------------')
     outputxt.writerow('Total Months: {months_total}')
     outputxt.writerow('Total: {net}')
-    outputxt.writerow('Averange change: {average_change}')
+    outputxt.writerow('Averange change: {round(average_change,2)}')
     outputxt.writerow('Greatest Increase in Profits: {gincrease_date} (${gincrease_prof})')
     outputxt.writerow('Greatest Decrease in Losses: {gdecrease_date} (${gdecrease_loss})')
 
